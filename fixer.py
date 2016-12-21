781dd7e3cf8232b5be1423952e8304a1229cd5cf
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-12-22 00:26:12 +0300

    Update py_fixer, add currency check and time check

--- fixer.py
+++ fixer.py
@@ -1,9 +1,23 @@
 import json
 import requests
+from datetime import datetime
 
 # TODO: Add tests
 
+# WARNING: Response date can be different with request date
+# req: 2010-01-03?base=EUR&symbols=EUR,RUB
+# {'date': '2009-12-31', 'base': 'EUR', 'rates': {'RUB': 43.154}}
+
+# There may be no information about currency on given day in history
+
 class Fixer:
+
+    date_format = '%Y-%m-%d'
+    min_date = datetime.strptime('1999-10-01', date_format)
+    allowed_curr = ['EUR', 'SEK', 'HRK', 'CHF', 'AUD', 'CAD', 'IDR', 'BRL', 'ILS',
+        'HKD', 'JPY', 'GBP', 'PHP', 'MYR', 'NZD', 'CNY', 'KRW', 'THB', 'USD', 'CZK',
+        'NOK', 'ZAR', 'TRY', 'DKK', 'BGN', 'MXN', 'HUF', 'PLN', 'RUB', 'RON', 'INR',
+        'SGD']
 
     def __init__(self, https=True):
         if https:
@@ -12,10 +26,22 @@ class Fixer:
             self.api_url = 'http://api.fixer.io/'
 
     def latest(self, base=None, symbols=None, date=None):
+        if date:
+            try:
+                conv_date = datetime.strptime(date, self.date_format)
+            except ValueError:
+                raise ValueError('Wrong date format, shoud be {}'.format(self.date_format))
+            if not (self.min_date <= conv_date <= datetime.today()):
+                raise ValueError('Too old/new date. Min date: {0} Max date(today): {1}'.format(self.min_date, datetime.today()))
         req = date if date else 'latest'
         if base:
+            if base not in self.allowed_curr:
+                raise ValueError("Currency {} is not supported".format(base))
             req += '?base={}'.format(base)
         if symbols:
+            for symb in symbols:
+                if symb not in self.allowed_curr:
+                    raise ValueError("Currency {} is not supported".format(base))
             req += '&' if base else '?'
             req += 'symbols={}'.format(','.join(symbols))
         return self._request(req)
@@ -31,6 +57,10 @@ class Fixer:
         return resp
 
 
-if __name__ == '__main__':
+def main():
     f = Fixer()
-    print(f.latest(base='EUR', symbols=['EUR', 'RUB'],  date='2010-01-03'))
+    print(f.latest())
+    print(f.latest(base='EUR', symbols=['EUR', 'RUB'],  date='2014-01-03'))
+
+if __name__ == '__main__':
+    main()
